6ef549c60f9b0a9df1da08daa0ed391be0f0139e
AnnaMariaSch <anna-maria-schmitt@gmx.de>
2025-03-20 08:22:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 function updateTime() {
   // LosAngeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement =
-    losAngelesElement.LosAngelesElemenentdocument.querySelector(".date");
-  let losAngelesTimeElement =
-    losAngelesElement.LosAngelesElemenentdocument.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  losAngelesDateElement.innerHTML = losAngelesTime.format(
-    "MMMM Do YYYY, h:mm:ss a"
-  );
-  losAngelesTimeElement.innerHTML = losAngelesTime
-    .moment()
-    .format("h:mm:ss [<small>]A[</small>]");
+  if (losAngelesElement) {
+    let losAngelesDateElement =
+      losAngelesElement.LosAngelesElemenentdocument.querySelector(".date");
+    let losAngelesTimeElement =
+      losAngelesElement.LosAngelesElemenentdocument.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Paris
   let parisElement = document.querySelector("#paris");
-  let parisDateElement =
-    parisElement.parisElemenentdocument.querySelector(".date");
-  let parisTimeElement =
-    parisElement.parisElemenentdocument.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-  parisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY, h:mm:ss a");
-  parisTimeElement.innerHTML = ParisTime.moment().format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  if (parisElement) {
+    let parisDateElement =
+      parisElement.parisElemenentdocument.querySelector(".date");
+    let parisTimeElement =
+      parisElement.parisElemenentdocument.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = ParisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
@@ -35,50 +37,21 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", "").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = cityTimeZone;
-  citiesElement = (
+  citiesElement.innerHTML = `
     <div class="city">
       <div>
-        <h2>${cityTimeZone}</h2>
+        <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
       </div>
       <div class="time">
         ${cityTime.format("h:mm:ss")} <small> ${cityTime.format("A")} </small>
       </div>
     </div>
-  );
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1);
 
-let citiesSelect = document.querySelector("#cities");
+let citiesSelect = document.querySelector("#city");
 citiesSelect.addEventListener("change", updateCity);
-
-// New York
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement =
-  losAngelesElement.LosAngelesElemenentdocument.querySelector(".date");
-let losAngelesTimeElement =
-  losAngelesElement.LosAngelesElemenentdocument.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
-losAngelesDateElement.innerHTML = losAngelesTime.format(
-  "MMMM Do YYYY, h:mm:ss a"
-);
-losAngelesTimeElement.innerHTML = losAngelesTime
-  .moment()
-  .format("h:mm:ss [<small>]A[</small>]");
-
-// Rome
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement =
-  losAngelesElement.LosAngelesElemenentdocument.querySelector(".date");
-let losAngelesTimeElement =
-  losAngelesElement.LosAngelesElemenentdocument.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
-losAngelesDateElement.innerHTML = losAngelesTime.format(
-  "MMMM Do YYYY, h:mm:ss a"
-);
-losAngelesTimeElement.innerHTML = losAngelesTime
-  .moment()
-  .format("h:mm:ss [<small>]A[</small>]");
